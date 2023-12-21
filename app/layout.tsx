21081df73500b9b9c1dac7e2ec81356bec1a5f04
Nausam <nausam.hussain@gmail.com>
2023-12-21 10:09:31 +0500

    First Commit

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Valsalva",
+  description:
+    "Valsalva is an elegent e-commerce website to buy snorkeling gear and equipment.",
+  icons: {
+    icon: "/asets/images/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ components/shared/Header.tsx
@@ -0,0 +1,42 @@
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import React from "react";
+import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
+
+const Header = () => {
+  return (
+    <header className="w-full borber-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <h2 className="font-bold text-3xl">Valsalva</h2>
+        </Link>
+
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
+          </SignedIn>
+          <SignedOut>
+            <Button
+              asChild
+              className="rounded-md bg-black hover:bg-slate-800"
+              size="lg"
+            >
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
