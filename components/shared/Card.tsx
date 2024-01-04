599360de54c63420567a16a20a330267c3b83de3
Nausam <nausam.hussain@gmail.com>
2024-01-04 12:45:45 +0500

    Attempt to fix home page load error

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -14,8 +14,8 @@ const Card = ({ product, hasOrderLink }: CardProps) => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
 
-  // const isProductCreator = product.creator._id === userId;
-  const isProductCreator = product.creator._id === userId.toString();
+  const isProductCreator = product.creator._id === userId;
+  // const isProductCreator = product.creator._id === userId.toString();
 
   return (
     <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]">
