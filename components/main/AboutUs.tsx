63833cf9fecb3279b3e496224d73871af6c415dc
Nausam <nausam.hussain@gmail.com>
2024-03-16 17:55:52 +0500

    Added suspense to AboutUs and Collection components

--- components/main/AboutUs.tsx
+++ components/main/AboutUs.tsx
@@ -5,24 +5,47 @@ import Link from "next/link";
 
 const AboutUs = () => {
   return (
-    <section className="wrapper flex flex-wrap gap-10 mt-28">
+    <section className="wrapper flex flex-wrap gap-10 md:mt-0 mt-28">
       <div className="grid grid-cols-1 gap-5 lg:grid-cols-2 2xl:gap-0 items-center justify-center ">
-        <Image
-          src="/assets/images/logo_white.png"
-          alt="hero"
-          width={250}
-          height={250}
-          className="object-contain object-center mx-auto invert dark:invert-0 hidden md:block"
-        />
-        <Image
-          src="/assets/images/logo_white.png"
-          alt="hero"
-          width={150}
-          height={150}
-          className="object-contain object-center mx-auto invert dark:invert-0 md:hidden block"
-        />
+        <div className="lg:hidden flex-col justify-center gap-8 flex">
+          <h2 className="h2-bold text-gray-800 dark:text-white text-start">
+            About Us
+          </h2>
+          <p className="p-regular-20 md:p-regular-18 text-gray-700 dark:text-gray-300 text-start">
+            Welcome to Valsalva, your premium destination for customizable
+            snorkel fins tailored to meet your exact preferences and needs. At
+            Valsalva, we are passionate about providing snorkel enthusiasts with
+            the ultimate experience in comfort, performance, and style.
+          </p>
+          <Button
+            size="lg"
+            asChild
+            className="button bg-black border border-black hover:bg-transparent text-white hover:text-black dark:bg-white dark:border-black dark:hover:border-white dark:text-black dark:hover:bg-transparent dark:hover:text-white  font-bold w-full sm:w-fit transition-all duration-300 ease-in-out shadow-lg mt-5"
+          >
+            <Link href="/product/65e9d07d684eaf43ce92ee67/custom">
+              Customize Now
+            </Link>
+          </Button>
+        </div>
 
-        <div className="flex flex-col justify-center gap-8 mt-16">
+        <div className="mt-20">
+          <Image
+            src="/assets/images/logo_white.png"
+            alt="hero"
+            width={250}
+            height={250}
+            className="object-contain object-center mx-auto invert dark:invert-0 hidden md:block"
+          />
+          <Image
+            src="/assets/images/logo_white.png"
+            alt="hero"
+            width={150}
+            height={150}
+            className="object-contain object-center mx-auto invert dark:invert-0 md:hidden block"
+          />
+        </div>
+
+        <div className="flex-col justify-center gap-8 mt-16 hidden lg:flex">
           <h2 className="h2-bold text-gray-800 dark:text-white text-start">
             About Us
           </h2>
--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -1,5 +1,5 @@
 import { IProduct } from "@/lib/database/models/product.model";
-import React from "react";
+import { Suspense } from "react";
 import Pagination from "./Pagination";
 import Card from "./Card";
 
@@ -27,45 +27,47 @@ const Collection = ({
 }: CollectionProps) => {
   return (
     <>
-      {data.length > 0 ? (
-        <div className="flex-center flex-col gap-10 ">
-          <ul className="flex-center w-full flex-wrap gap-5 xl:gap-10">
-            {data.map((product, index) => {
-              const hasOrderLink = collectionType === "My_Products";
-              const hidePrice = collectionType === "My_Products";
+      <Suspense fallback={<div>Loading...</div>}>
+        {data.length > 0 ? (
+          <div className="flex-center flex-col gap-10 ">
+            <ul className="flex-center w-full flex-wrap gap-5 xl:gap-10">
+              {data.map((product, index) => {
+                const hasOrderLink = collectionType === "My_Products";
+                const hidePrice = collectionType === "My_Products";
 
-              return (
-                <li key={index} className="flex justify-center">
-                  <Card
-                    product={product}
-                    hasOrderLink={hasOrderLink}
-                    hidePrice={hidePrice}
-                  />
-                </li>
-              );
-            })}
-          </ul>
+                return (
+                  <li key={index} className="flex justify-center">
+                    <Card
+                      product={product}
+                      hasOrderLink={hasOrderLink}
+                      hidePrice={hidePrice}
+                    />
+                  </li>
+                );
+              })}
+            </ul>
 
-          {!homePage === true && totalPages > 1 && (
-            <Pagination
-              urlParamName={urlParamName}
-              page={page}
-              totalPages={totalPages}
-            />
-          )}
-        </div>
-      ) : (
-        <div className="flex-center wrapper min-h-[200px] w-full flex-col gap-3 rounded-sm bg-grey-50 dark:bg-[#191919] py-28 text-center ">
-          <h3 className="p-bold-20 md:h5-bold dark:text-gray-300">
-            {" "}
-            {emptyTitle}{" "}
-          </h3>
-          <p className="p-regular-14 dark:text-gray-400">
-            {" "}
-            {emptyStateSubtext}{" "}
-          </p>
-        </div>
-      )}
+            {!homePage === true && totalPages > 1 && (
+              <Pagination
+                urlParamName={urlParamName}
+                page={page}
+                totalPages={totalPages}
+              />
+            )}
+          </div>
+        ) : (
+          <div className="flex-center wrapper min-h-[200px] w-full flex-col gap-3 rounded-sm bg-grey-50 dark:bg-[#191919] py-28 text-center ">
+            <h3 className="p-bold-20 md:h5-bold dark:text-gray-300">
+              {" "}
+              {emptyTitle}{" "}
+            </h3>
+            <p className="p-regular-14 dark:text-gray-400">
+              {" "}
+              {emptyStateSubtext}{" "}
+            </p>
+          </div>
+        )}
+      </Suspense>
     </>
   );
 };
